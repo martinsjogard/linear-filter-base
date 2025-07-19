9ad772345a13808cd6fafb0db787e55be0168d6f
Martin Sjøgård (Sjoegaard) <martinsjogard@gmail.com>
2025-07-19 12:01:25 -0400

    Update sig_slow_modulation.py

--- sig_slow_modulation.py
+++ sig_slow_modulation.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.signal import hilbert
 
-def sig_slow_modulation(Z, fcenter):
+def msfun_sig_slow_modulation(Z, fcenter):
     """
     Remove fast oscillation at fcenter from signal Z, keeping only slow modulations.
 
